21d40441df0c72dfc04ade6b0a5ef514adf7e87b
caimao feng <fcmfcm01@qq.com>
2020-07-23 22:07:04 +0800

    会议安排问题
    Signed-off-by: caimao feng <fcmfcm01@qq.com>

--- src/main/java/org/fcm/alg/example/chapter2/MeetingArrangement.java
+++ src/main/java/org/fcm/alg/example/chapter2/MeetingArrangement.java
@@ -4,12 +4,16 @@ import lombok.Data;
 import lombok.ToString;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 某跨国公司总裁分身乏术，正为一大堆会议时间表焦头烂额，希望秘书能够做出合理的安排，能在有限的时间内召开更多的会议
  */
 public class MeetingArrangement {
+    private static AtomicInteger steps = new AtomicInteger(0);
+
     private static Meeting[] init(int count) {
         Meeting[] meetings = new Meeting[count];
         Random random = new Random();
@@ -27,6 +31,7 @@ public class MeetingArrangement {
         Meeting pickedMeeting = meetings[0];
         System.out.println(pickedMeeting.toString());
         for (int i = 0; i < meetings.length; i++) {
+            steps.incrementAndGet();
             if (pickedMeeting.getEnd() <= meetings[i].getStart()) {
                 arrangeMeeting(Arrays.copyOfRange(meetings, i, meetings.length));
                 break;
@@ -34,11 +39,34 @@ public class MeetingArrangement {
         }
     }
 
+    public static void arrangeMeetingWithoutRecursive(Meeting[] meetings) {
+
+        Meeting[] meetingsPicked = new Meeting[meetings.length];
+        meetingsPicked[0] = meetings[0];
+        Meeting pickedMeeting = meetings[0];
+        int last = pickedMeeting.getEnd();
+        for (int i = 0; i < meetings.length; i++) {
+            steps.incrementAndGet();
+            if (last <= meetings[i].getStart()) {
+                last = meetings[i].getEnd();
+                meetingsPicked[i] = meetings[i];
+            }
+        }
+        System.out.println("Piked meetings = ");
+        Arrays.stream(meetingsPicked)
+                .filter(Objects::nonNull)
+                .forEach(meeting -> System.out.println(meeting.toString()));
+    }
+
     public static void main(String[] args) {
         Meeting[] meetings = init(10);
         Arrays.sort(meetings);
         System.out.println("Meetings = " + Arrays.deepToString(meetings));
         arrangeMeeting(meetings);
+        System.out.println("recursive " + steps.intValue() + " steps in total.");
+        steps = new AtomicInteger(0);
+        arrangeMeetingWithoutRecursive(meetings);
+        System.out.println("Without recursive " + steps.intValue() + " steps in total.");
 
     }
 }
